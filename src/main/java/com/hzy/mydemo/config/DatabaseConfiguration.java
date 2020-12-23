2b22937589990a2d294e11d4e7e67e6b6122c705
hanzongyan <hanzongyan0106@163.com>
2020-12-23 14:30:34 +0800

    引入QueryDSL查询框架

--- src/main/java/com/hzy/mydemo/config/DatabaseConfiguration.java
+++ src/main/java/com/hzy/mydemo/config/DatabaseConfiguration.java
@@ -1,12 +1,16 @@
 package com.hzy.mydemo.config;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManager;
 
 
 @Configuration
@@ -16,4 +20,9 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class DatabaseConfiguration {
 
     private final Logger log = LoggerFactory.getLogger(DatabaseConfiguration.class);
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
+        return new JPAQueryFactory(entityManager);
+    }
 }
