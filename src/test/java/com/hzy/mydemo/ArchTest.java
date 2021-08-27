6cd79c4698d803f87bb4d857beeba0bf975faa43
韩宗晏 <hanzongyan0106@163.com>
2021-08-27 14:28:21 +0800

    Initial version of demo generated by JHipster-7.0.1

---
+++ src/test/java/com/hzy/mydemo/ArchTest.java
@@ -0,0 +1,29 @@
+package com.hzy.mydemo;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.core.importer.ImportOption;
+import org.junit.jupiter.api.Test;
+
+class ArchTest {
+
+    @Test
+    void servicesAndRepositoriesShouldNotDependOnWebLayer() {
+        JavaClasses importedClasses = new ClassFileImporter()
+            .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS)
+            .importPackages("com.hzy.mydemo");
+
+        noClasses()
+            .that()
+            .resideInAnyPackage("com.hzy.mydemo.service..")
+            .or()
+            .resideInAnyPackage("com.hzy.mydemo.repository..")
+            .should()
+            .dependOnClassesThat()
+            .resideInAnyPackage("..com.hzy.mydemo.web..")
+            .because("Services and repositories should not depend on web layer")
+            .check(importedClasses);
+    }
+}
